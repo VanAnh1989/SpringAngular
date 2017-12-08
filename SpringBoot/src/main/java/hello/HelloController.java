085603f69c774843f7bb17eb96e23ad86fc5f23a
VanAnh1989 <anhnvd00205@fot.edu.vn>
2017-12-08 11:48:25 +0700

    done

--- SpringBoot/src/main/java/hello/HelloController.java
+++ SpringBoot/src/main/java/hello/HelloController.java
@@ -1,14 +1,18 @@
 package hello;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
     
     @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    public ModelAndView index() {
+    	System.out.print("done");
+    	System.out.print("OK");
+    	
+        return new ModelAndView("index");
     }
     
 }
